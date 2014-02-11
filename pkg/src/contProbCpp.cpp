34772a3eaa402fe377a678496f97b6380854cb00
oyvble <oyvble@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-11 13:22:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mastermix@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/contProbCpp.cpp
+++ pkg/src/contProbCpp.cpp
@@ -340,7 +340,85 @@ extern "C" {
  for(i=0; i<*nL; i++) {
   delete lociList[i]; //delete objects
  }
- delete[] CnA,CnA2,CGind,CPGind,nCG,nAsq;
+ delete[] CnA;
+ delete[] CnA2;
+ delete[] CGind;
+ delete[] CPGind;
+ delete[] nCG;
+ delete[] nAsq;
+}
+
+
+//function for calculating inner product of integrale of theta
+void calcLthetaC(double *PE,int *nA, int *nC, int *nL, double *allY,  double *allX,  double *allO,  double *alliW,  int *cdfX, int *nQi, double *pG, double *theta) {
+
+ int i,j,cc;
+ int *CnA = new int[*nL+1]; //cumulative index for vector
+ int *CnA2 = new int[*nL+1]; //cumulative index for matrix (vectorized matrix)
+ int *nAsq = new int[*nL+1]; //squared numbers of nA
+ int *CnQ = new int[*nL+1]; //cumulative index for matrix (vectorized matrix)
+
+ Mat<double> *Yi; 
+ Mat<double> *iWi; 
+ Mat<double> *Xij; 
+ Mat<double> *Oij; 
+ Mat<double> *omega; //mixture proportion 
+ omega = new Mat<double>( theta, *nC-1, 1 ,false); //insert mixture proportion
+
+ //temporary variables:
+  Mat<double> mui;
+  Mat<double> ri;
+  Mat<double> Di;
+  double bigsum;
+  double sigma;
+  sigma = sqrt(theta[*nC-1]);
+
+ for(i=0; i<*nL; i++) { 
+  nAsq[i] = nA[i]*nA[i]; 
+ } //square numbers
+ cumsum(nA, nL, CnA); //cumulative numbers of nA
+ cumsum(nAsq, nL, CnA2); //cumulative squared number of alleles nA
+ cumsum(nQi, nL, CnQ); //cumulative of numbers of genotypes. Used in pG
+
+#ifdef DEBUG
+ for(i=0; i<*nL; i++) {
+  printf("nA[%i]=%d\n",i,nA[i]);
+  printf("CnA[%i]=%d\n",i,CnA[i]);
+  printf("CnA2[%i]=%d\n",i,CnA2[i]);
+  printf("nQi[%i]=%d\n",i,nQi[i]);
+  for(j=0; j<nA[i]; j++) { printf("Y[%d,%d]=%f\n",i,j,allY[CnA[i]+j]); }
+  printf("\n"); 
+ }
+#endif
+
+ //calculating probabilities on the move:
+ cc = 0; //counter for X-matrix and O-matrix
+ for(i=0; i<*nL; i++) {
+  Yi =  new Mat<double>( &allY[CnA[i]], nA[i], 1,false); //init datavector
+  iWi = new Mat<double>( &alliW[CnA2[i]],nA[i],nA[i],false); //init. covariance-matrix
+  bigsum = 0.0; //sum over all genotypes
+
+  for(j=0; j<nQi[i]; j++) { //for all genotypes: Init space and insert relevant matrice directly
+   Xij = new Mat<double>( &allX[cdfX[cc]-1] ,nA[i],*nC-1,false); //init. Xij-matrix
+   Oij = new Mat<double>( &allO[cdfX[cc]-1] ,nA[i],*nC-1,false); //init. Pi-matrices as blocked   
+   mui = (*Xij)*(*omega) + *Oij; //mean
+   ri = *Yi - mui; //residual
+   Di = trans(ri)*(*iWi)*(ri); //mahalonobis distance
+   bigsum = bigsum + exp(-0.5/theta[*nC-1]*(Di.at(0,0)))/pow(sigma,nA[i])*pG[CnQ[i]+j]; //#likelihood value   
+   cc = cc + 1; //update counter
+   delete Xij;
+   delete Oij;
+  }//end for each j: genotype combination
+  *PE = (*PE)*bigsum;
+  delete Yi;
+  delete iWi;
+ }//end for each loci i:
+
+ delete[] CnA;
+ delete[] CnA2;
+ delete[] CnQ;
+ delete[] nAsq;
+ delete omega;
 }
 
 } 
